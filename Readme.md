cd0e116d6d1503b4d5ad567fa87668d99e98f7d9
= <=>
2020-09-20 00:40:00 +0530

    endpoint fetch-nearby configured

---
+++ Readme.md
@@ -0,0 +1,5 @@
+1. Change the key in .env file with gooogle places API key
+2. run npm install
+3. Run the code using node index.js
+4. visit API fetch-nearby (GET request) passing lat, lng and radius(optional default is 500meters) as query parameters
+5. The API will return top 20 restraurants nearby within the radius of 500 meters specified otherwise
--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const fetch = require('node-fetch');
+
 const app = express();
 
 dotenv.config();
 const port = process.env.PORT || 4000;
+const key = process.env.KEY;
 
 app.use(cors());
+
+app.get('/fetch-nearby',async (req,res)=>{
+    const {lat,lng,radius = 500} = req.query;
+    
+    if(!lat || !lng){
+        return res.status(401).json({
+            message:'invalid params'
+        });
+    }
+
+    try{
+        const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=restaurant&key=${key}`;
+        const response = await fetch(url);
+        if(response.ok){
+            const json = await response.json();
+            if(json.status == 'OK'){
+                return res.status(200).json({
+                    data: json.results
+                });
+            }else{
+                return res.status(200).json({
+                    message: 'Some error while fetching places'
+                });
+            }
+        }else{
+            res.status(500).json({
+                message:'Internal Server Error'
+            });
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({
+            message:'Internal Server Error'
+        });
+    }
+})
 
 app.listen(port,()=>{
     console.log('Server running on ' + port);
